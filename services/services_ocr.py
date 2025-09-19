199ca10af390475e666a1a848d1296671cd813c8
Vũ Đức Chính <chinh_vd@ehicas.vn>
2025-09-19 15:10:26 +0700

    fix error size image respone > 4000

--- services/services_ocr.py
+++ services/services_ocr.py
@@ -12,16 +12,6 @@ def infer_image_from_base64(payload: ImageBase64Input, request: Request):
     try:
         image_data = base64.b64decode(payload.image_base64)
         image = Image.open(io.BytesIO(image_data)).convert("RGB")
-        w, h = image.size
-        max_dim = 4000
-        if max(w, h) > max_dim:
-            if w >= h:
-                new_w = max_dim
-                new_h = int(h * (max_dim / w))
-            else:
-                new_h = max_dim
-                new_w = int(w * (max_dim / h))
-            image = image.resize((new_w, new_h), Image.LANCZOS)
         image_np = np.array(image)
         model = request.app.state.model
         results = model.predict(
